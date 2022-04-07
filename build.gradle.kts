5a5f73d7f60c69ffb976b6fe63e97318e525b0b8
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2022-04-07 16:18:44 +0200

    Remove download asset during promote

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ buildscript {
         mavenLocal()
     }
     dependencies {
-        classpath("com.saagie:technologiesplugin:1.3.4")
+        classpath("com.saagie:technologiesplugin:1.3.5")
     }
 }
 apply<SaagieTechnologiesPackageGradlePlugin>()
